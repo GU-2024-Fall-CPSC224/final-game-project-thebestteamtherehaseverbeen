38f416dfbc210582dd34a0256cdd1aa9b3ca0a6b
ADH-17 <ayden.humphries17@gmail.com>
2024-11-13 11:17:51 -0800

    Added back the imports to Artillery

--- src/main/java/edu/gonzaga/Artillery.java
+++ src/main/java/edu/gonzaga/Artillery.java
@@ -1,4 +1,6 @@
 package edu.gonzaga;
+import java.util.ArrayList;
+import java.util.Scanner;
 /*
  * This class holds all the code under main. This is where the stuff under the abstraction happens.
  */
